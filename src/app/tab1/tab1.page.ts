a8231676982ceefbe15fd658b1a75ffc0918e7a3
Dan-Calm <daniel.calderonm95@gmail.com>
2025-04-10 23:45:51 -0400

    firebase

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../config/firebase.config'; // Importa la configuración de Firebase
+// import { db } from '../config/firebase.config'; // Importa la configuración de Firebase
 
 @Component({
   selector: 'app-tab1',
@@ -20,7 +20,7 @@ export class Tab1Page implements OnInit {
   // Función para obtener las cartas desde Firebase
   async obtenerCartasDesdeFirebase() {
     try {
-      const referenciaColeccion = collection(db, 'productos_afkstore'); // Referencia a la colección en Firestore
+      const referenciaColeccion = collection(db, 'productos_afkstore'); // Nombre de la colección productos_afkstore
       const resultadoConsulta = await getDocs(referenciaColeccion); // Obtener los documentos de la colección
       this.listaDeCartas = resultadoConsulta.docs.map((documento) => 
         documento.data() as { nombre: string; precio: string; disponibilidad: string; imagen: string }
